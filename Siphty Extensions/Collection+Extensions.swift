1235f716923ad16e97025ce09884938ebd358436
Yi JIANG <jiang.yi@siphty.com>
2019-01-26 22:24:50 +1100

    Update pods

--- Siphty Extensions/Collection+Extensions.swift
+++ Siphty Extensions/Collection+Extensions.swift
@@ -8,14 +8,8 @@
 
 import Foundation
 
-extension Collection where Indices.Iterator.Element == Index, Index == Int {
+extension Collection where Indices.Iterator.Element == Index {
     public subscript (safe index: Index) -> Iterator.Element? {
-        return index < self.count ? self[index] : nil
-    }
-}
-
-extension Collection where Indices.Iterator.Element == Index, Index == IndexPath {
-    public subscript (safe index: IndexPath) -> Iterator.Element? {
         return  indices.contains(index) ? self[index] : nil
     }
 }
